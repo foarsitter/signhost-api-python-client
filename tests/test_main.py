a8b9ba266867b066368004ce5f14201869f22e55
Jelmer Draaijer <info@jelmert.nl>
2023-02-13 09:34:45 +0100

    fix: env

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,6 +5,7 @@ import pytest
 from click.testing import CliRunner
 
 from signhost import __main__
+from signhost.__main__ import transaction
 
 
 @pytest.fixture
@@ -17,8 +18,9 @@ def test_main_succeeds(runner: CliRunner, mocked_api: None) -> None:
     """It exits with a status code of zero."""
 
     with tempfile.NamedTemporaryFile() as temp:
-
         result = runner.invoke(
-            __main__.main, ["transaction", str(temp.name), "test@pytest.io", "--yes"]
+            __main__.main,
+            ["transaction", str(temp.name), "test@pytest.io", "--yes"],
+            env={"SIGNHOST_API_KEY": "test1234", "SIGNHOST_APP_KEY": "test1234"},
         )
         assert result.exit_code == 0, result
