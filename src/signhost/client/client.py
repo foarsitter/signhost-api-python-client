959e222516cdd67ba4e2aeb869fb0f24dd1a85b7
Jelmer <info@jelmert.nl>
2023-04-21 13:59:23 +0200

    Async support (#71)
    * feat: async implementation

--- src/signhost/client/client.py
+++ src/signhost/client/client.py
@@ -4,7 +4,9 @@ import io
 from json import JSONDecodeError
 from struct import pack
 from typing import Any
+from typing import AsyncIterator
 from typing import Dict
+from typing import Mapping
 from typing import Type
 from typing import TypeVar
 
@@ -39,10 +41,18 @@ class BaseClient:
         self.app_key = app_key
         self.base_url = base_url
 
+    def create_content_headers(self, data: str) -> Mapping[str, str]:
+        content_length: str = str(len(data))
+        content_type: str = "application/json"
+        headers: Mapping[str, str] = {
+            "Content-Length": content_length,
+            "Content-Type": content_type,
+        }
+        return headers
+
     def process_response(
         self, response: httpx.Response, model: Type[_T], status_code_success: int = 200
     ) -> _T:
-
         try:
             if response.status_code == status_code_success:
                 response_json = response.json()
@@ -55,7 +65,6 @@ class BaseClient:
             ) from e
 
     def create_error(self, response: httpx.Response) -> errors.SignhostError:
-
         try:
             response_json = response.json()
         except JSONDecodeError:
@@ -77,11 +86,18 @@ class BaseClient:
         return exception_type
 
     def authenticate_request(self, request: httpx.Request) -> httpx.Request:
-
         request.headers["Authorization"] = f"APIKey {self.api_key}"
         request.headers["Application"] = f"APPKey {self.app_key}"
 
         return request
+
+
+def b64_digest(file_content: io.IOBase) -> str:
+    sha = hashlib.sha256(file_content.read())
+    digest = sha.digest()
+    b64_digest = base64.b64encode(pack(f"{len(digest)}s", digest)).decode("utf-8")
+    file_content.seek(0)
+    return b64_digest
 
 
 class DefaultClient(BaseClient):
@@ -151,9 +167,7 @@ class DefaultClient(BaseClient):
         """POST /api/transaction"""
 
         data = transaction.json()
-        content_length = str(len(data))
-        content_type = "application/json"
-        headers = {"Content-Length": content_length, "Content-Type": content_type}
+        headers = self.create_content_headers(data)
 
         response = self.client.post("transaction", content=data, headers=headers)
 
@@ -163,18 +177,14 @@ class DefaultClient(BaseClient):
         self, transaction_id: str, file_id: str, file_content: io.IOBase
     ) -> bool:
         """PUT /api/transaction/{transactionId}/file/{fileId}"""
-        sha = hashlib.sha256(file_content.read())
-        digest = sha.digest()
-        b64_digest = base64.b64encode(pack(f"{len(digest)}s", digest)).decode("utf-8")
-
-        file_content.seek(0)
+        digest: str = b64_digest(file_content)
 
         response = self.client.put(
             f"transaction/{transaction_id}/file/{file_id}",
             content=file_content,
             headers={
                 "Content-Type": "application/pdf",
-                "Digest": f"SHA256={b64_digest}",
+                "Digest": f"SHA256={digest}",
             },
         )
 
@@ -201,3 +211,116 @@ class AsyncClient(BaseClient):
     """Asynchronous client."""
 
     client: httpx.AsyncClient
+
+    def __init__(
+        self,
+        api_key: str,
+        app_key: str,
+        base_url: str = "https://api.signhost.com/api/",
+        **httpx_kwargs: Any,
+    ):
+        super().__init__(api_key, app_key, base_url)
+        self.client = self.create_client(base_url, **httpx_kwargs)
+
+    def create_client(self, base_url: str, **httpx_kwargs: Any) -> httpx.AsyncClient:
+        httpx_kwargs.setdefault("base_url", base_url)
+        httpx_kwargs.setdefault("auth", self.authenticate_request)
+        return httpx.AsyncClient(**httpx_kwargs)
+
+    async def transaction_get(self, transaction_id: str) -> models.Transaction:
+        """GET /api/transaction/{transactionId}"""
+        response = await self.client.get(f"transaction/{transaction_id}")
+
+        return self.process_response(response, models.Transaction)
+
+    async def transaction_cancel(
+        self, transaction_id: str, send_notifications: bool = False, reason: str = ""
+    ) -> models.Transaction:
+        """DELETE /api/transaction/{transactionId}"""
+        response = await self.client.request(
+            "DELETE",
+            f"transaction/{transaction_id}",
+            json={"SendNotifications": send_notifications, "Reason": reason},
+        )
+        return self.process_response(response, models.Transaction)
+
+    async def transaction_file_get(self, transaction_id: str, file_id: str) -> bytes:
+        """
+        GET /api/transaction/{transactionId}/file/{fileId}
+
+        Returns the contents of the (signed) document(s).
+        """
+        response = await self.client.get(f"transaction/{transaction_id}/file/{file_id}")
+
+        if response.status_code == httpx.codes.OK:
+            return response.content
+
+        raise self.create_error(response)
+
+    async def receipt_get(self, transaction_id: str) -> bytes:
+        """
+        GET /api/file/receipt/{transactionId}
+
+        Returns the contents of the receipt when the transaction is successfully signed (Status=30)
+        """
+        response = await self.client.get(f"file/receipt/{transaction_id}")
+
+        if response.status_code == httpx.codes.OK:
+            return response.content
+
+        raise self.create_error(response)
+
+    async def transaction_init(
+        self, transaction: models.Transaction
+    ) -> models.Transaction:
+        """POST /api/transaction"""
+        data: str = transaction.json()
+
+        headers = self.create_content_headers(data)
+
+        response = await self.client.post("transaction", content=data, headers=headers)
+
+        return self.process_response(response, models.Transaction)
+
+    async def transaction_file_put(
+        self, transaction_id: str, file_id: str, file_content: io.IOBase
+    ) -> bool:
+        """PUT /api/transaction/{transactionId}/file/{fileId}"""
+        digest: str = b64_digest(file_content)
+
+        response = await self.client.put(
+            f"transaction/{transaction_id}/file/{file_id}",
+            content=bytes_as_stream(file_content),
+            headers={
+                "Content-Type": "application/pdf",
+                "Digest": f"SHA256={digest}",
+            },
+        )
+
+        if response.status_code in [
+            httpx.codes.CREATED,
+            httpx.codes.ACCEPTED,
+            httpx.codes.NO_CONTENT,
+        ]:
+            return True
+
+        raise self.create_error(response)
+
+    async def transaction_start(self, transaction_id: str) -> bool:
+        """PUT /api/transaction/{transactionId}/start"""
+        response = await self.client.put(f"transaction/{transaction_id}/start")
+
+        if response.status_code != httpx.codes.NO_CONTENT:
+            raise self.create_error(response)
+
+        return True
+
+
+CHUNK_SIZE = 65_536
+
+
+async def bytes_as_stream(file_content: io.IOBase) -> AsyncIterator[bytes]:
+    chunk = file_content.read(CHUNK_SIZE)
+    while chunk:
+        yield chunk
+        chunk = file_content.read(CHUNK_SIZE)
